5de025bf4b96304978c6c214ddf34c2a979eb260
Alexey Ragozin <alexey.ragozin@gmail.com>
2014-02-06 01:35:52 +0400

    Detect build fail in tests

--- src/test/java/org/gridkit/lab/gridant/AntRunTest.java
+++ src/test/java/org/gridkit/lab/gridant/AntRunTest.java
@@ -8,6 +8,8 @@ import java.net.Socket;
 
 import junit.framework.Assert;
 
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildListener;
 import org.apache.tools.ant.launch.LaunchException;
 import org.junit.Assume;
 import org.junit.Test;
@@ -86,10 +88,61 @@ public class AntRunTest {
 	}
 		
 	private void runLocalTarget(String target) throws MalformedURLException, LaunchException {
+		errorMessage = null;
 		Assert.assertEquals(0, MockLauncher.main("-buildfile", "src/test/resources/local-test-script.xml", target));	
+		if (errorMessage != null) {
+			Assert.fail(errorMessage);
+		}
 	}	
 
 	private void runRemoteTarget(String target) throws MalformedURLException, LaunchException {
-		Assert.assertEquals(0, MockLauncher.main("-buildfile", "src/test/resources/remote-test-script.xml", target));	
+		errorMessage = null;
+		Assert.assertEquals(0, MockLauncher.main("-listener", Listener.class.getName(), "-buildfile", "src/test/resources/remote-test-script.xml", target));
+		if (errorMessage != null) {
+			Assert.fail(errorMessage);
+		}
 	}	
+
+	static String errorMessage; 
+	
+	public static class Listener implements BuildListener {
+
+		
+		@Override
+		public void buildStarted(BuildEvent event) {
+			// ignore
+		}
+
+		@Override
+		public void buildFinished(BuildEvent event) {
+			if (event.getException() != null) {
+				errorMessage = event.getException().toString();
+			}
+		}
+
+		@Override
+		public void targetStarted(BuildEvent event) {
+			// ignore
+		}
+
+		@Override
+		public void targetFinished(BuildEvent event) {
+			// ignore
+		}
+
+		@Override
+		public void taskStarted(BuildEvent event) {
+			// ignore
+		}
+
+		@Override
+		public void taskFinished(BuildEvent event) {
+			// ignore
+		}
+
+		@Override
+		public void messageLogged(BuildEvent event) {
+			// ignore
+		}
+	}
 }
